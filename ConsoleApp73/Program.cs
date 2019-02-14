0cfbb8397724f5f8d00777bd8f0588d6ab3ea9b8
utvecklare.mattias@gmail.com <utvecklare.mattias@gmail.com>
2019-02-14 14:41:21 +0100

    Add project files.

---
+++ ConsoleApp73/Program.cs
@@ -0,0 +1,121 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace PrudoctMenu
+{
+    class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            List<lemonprice> product = new List<lemonprice>();
+
+        
+
+            bool keepAlive = true;
+
+            while (keepAlive)
+            {
+                Console.WriteLine(
+                    "--- Menu ---\n" +
+                    "1: Lemmon\n" +
+                    "2: Candy\n" +
+                    "3: Chips\n" +
+                    "0: Exit program"
+                    );
+                int selection = AskUserForNumberX("Select");
+
+                switch (selection)
+                {
+                    case 1:
+                        lemonprice();
+                        break;
+                    case 2:
+                     
+                        break;
+                    
+                   
+                    case 0:
+                        keepAlive = false;
+                    
+                        break;
+                    default:
+                        Console.WriteLine("Not a valid selection.");
+                        break;
+                }
+                Console.WriteLine("Press any key to continue.");
+                Console.ReadKey(true);
+                Console.Clear();
+            }
+        }
+
+        private static void lemonprice()
+        {
+            throw new NotImplementedException();
+        }
+
+        static lemonprice Product()
+        {
+            string Produkt = AskUserForX("Choice Product");
+          
+
+            return Product();
+        }// CreatePerson ends
+
+        static string AskUserForX(string x)
+        {
+            string input = "";
+
+            while (input.Length == 0)
+            {
+                Console.Write("Pleace input " + x + ": ");
+                input = Console.ReadLine();
+            }
+
+            return input;
+        }
+
+        static int AskUserForNumberX(string x)
+        {
+            int number = 0;
+            bool noNumber = true;
+
+            while (noNumber)
+            {
+                try
+                {
+                    number = Convert.ToInt32(AskUserForX(x));
+                    noNumber = false;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Not a number, Pleace try once more.");
+                }
+            }
+
+            return number;
+        }
+    }
+
+
+
+        public lemon()
+        {
+            int price =10;
+            int antal;
+            int summa ;
+
+            Console.Write("How many do you want?");
+            antal = Console.Read();
+            summa = (price * antal);
+           
+        return summa;
+        }
+
+    void lemonprice()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ ConsoleApp73/produkt.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrudoctMenu
+{
+    class lemonprice
+    {
+        static int countId = 0;
+
+        public int Id { get; private set; }
+
+        public string lemmon { get; set; }
+        public string chocklad { get; set; }
+        public string cips { get; set; }
+        int price;
+        public int Price {
+            get { return Price; }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("You most press 1-4");
+                }
+                else
+                {
+            
+                     
+                }
+            }
+        }
+        static int AskUserForsumma(int summan)
+        {
+            int price = 10;
+            int antal;
+            int summa;
+
+            Console.Write("How many do you want?");
+            antal = Console.Read();
+            summa = price * antal;
+            return summa;
+
+        }
+
+        public lemonprice() { }   
+
+        public lemonprice(string drink, string choice)    // When we Overload a Constructor or Method, it the input types that are the key for the computer to see the diffrence between them(not the verible name).
+        {
+            Id = ++countId;
+            lemmon = drink;
+            this.lemmon = choice;
+            return;
+        }
+
+        public lemonprice(int id) 
+        {
+           
+            this.Id = Id;
+        }
+
+     
+
+        override public string ToString()
+        {
+            return Id + ": ";
+        }
+    }
+}
